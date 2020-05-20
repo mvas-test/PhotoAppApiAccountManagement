15ebbb5afa32e1a9d21b87f7c3872ea3228dea4e
Michail Vasilakopoulos <bouzouki2015@gmail.com>
2020-05-20 13:20:01 +0100

    first commit

---
+++ src/main/java/com/appsdeveloper/photoapp/api/accountmanagement/PhotoAppApiAccountManagementApplication.java
@@ -0,0 +1,13 @@
+package com.appsdeveloper.photoapp.api.accountmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppApiAccountManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppApiAccountManagementApplication.class, args);
+	}
+
+}
