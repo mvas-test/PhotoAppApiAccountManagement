e42237a8c25db1ea75979541bcc4edbf11bbe5c3
Michail Vasilakopoulos <bouzouki2015@gmail.com>
2020-06-06 11:07:50 +0100

    Improved AccountManagmentController status check output info

--- src/main/java/com/appsdeveloper/photoapp/api/accountmanagement/controllers/AccountManagementController.java
+++ src/main/java/com/appsdeveloper/photoapp/api/accountmanagement/controllers/AccountManagementController.java
@@ -1,5 +1,7 @@
 package com.appsdeveloper.photoapp.api.accountmanagement.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/account")
 public class AccountManagementController {
 	
+	@Autowired
+	private Environment env;
+	
 	@GetMapping("/status/check")
 	public String status() {
-		return "Account service is Working";
+		return "Account service is Workingon on port:" + env.getProperty("local.server.port");
 	}
 
 }
